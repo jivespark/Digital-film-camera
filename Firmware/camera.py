9703e780a36d1a082e58f0e2a39820e97f4f96d5
Bence Topa <bencetopa06@gmail.com>
2026-04-09 11:43:12 +0200

    Add initial implementation of buzzer, camera, main, and storage modules

---
+++ Firmware/camera.py
@@ -0,0 +1,33 @@
+from picamera2 import Picamera2
+import time
+
+class Camera:
+    def __init__(self):
+        self.cam=Picamera2()
+        self.ev=0.0
+        self.ready=False
+        self._setup()
+    
+    def _setup(self):
+        config=self.cam.create_still_configuration(
+            main={"size": (4608, 2592)},
+            raw={"size": (4608, 2592)}
+        )
+        self.cam.configure(config)
+        self.cam.set_controls({
+            "AeEnable":True,
+            "ExposureValue":self.ev,
+            "AwbEnable": True,
+        })
+        self.cam.start()
+        time.sleep(2)
+        self.ready=True
+    
+    def capture(self, filepath, filename):
+        if(self.ready):
+            self.ready=False
+            self.cam.capture_file(filepath, name=filename)
+            self.ready=True
+    
+    def stop(self):
+        self.cam.stop()
